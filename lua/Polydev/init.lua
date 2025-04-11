7d12d97bf4e39935076bcf1f90b5958552796f08
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-04-11 19:38:56 -0400

    Adding globals to list to see if that fixes the issues

--- lua/Polydev/init.lua
+++ lua/Polydev/init.lua
@@ -8,16 +8,17 @@ M.lua = require("Polydev.lua")
 M.python = require("Polydev.python")
 M.rust = require("Polydev.rust")
 M.html = require("Polydev.html")
+M.globals = require("Polydev.globals")
 
 function M.load_language_module(lang)
     if M.loaded_languages[lang] then return true end
 
     local ok, lang_module = pcall(require, "Polydev." .. lang)
     if ok and lang_module.setup then
-        local opts = M.language_opts and M.language_opts[lang] or {}
-        lang_module.setup(opts)
-        M.loaded_languages[lang] = lang_module
-        return true
+	local opts = M.language_opts and M.language_opts[lang] or {}
+	lang_module.setup(opts)
+	M.loaded_languages[lang] = lang_module
+	return true
     end
 
     return false
@@ -25,13 +26,13 @@ end
 
 function M.create_project()
     vim.ui.input({ prompt = "Enter language for project: " }, function(lang)
-        if not lang or lang == "" then return print("Project creation canceled.") end
+	if not lang or lang == "" then return print("Project creation canceled.") end
 
-        if M.load_language_module(lang) and M.loaded_languages[lang].create_project then
-            M.loaded_languages[lang].create_project()
-        else
-            print("Error: No project creation method for " .. lang)
-        end
+	if M.load_language_module(lang) and M.loaded_languages[lang].create_project then
+	    M.loaded_languages[lang].create_project()
+	else
+	    print("Error: No project creation method for " .. lang)
+	end
     end)
 end
 
@@ -52,18 +53,18 @@ function M.setup(opts)
 
     -- Send config to globals
     require("Polydev.globals").setup(opts.globals or {})
-     M.language_opts = opts  -- store all opts, including language-specific one
+    M.language_opts = opts  -- store all opts, including language-specific one
 
     vim.api.nvim_create_user_command("NewProject", M.create_project, {})
     vim.api.nvim_create_user_command("NewFile", M.create_new_file, {})
     vim.keymap.set("n", "<leader>np", ":NewProject<CR>", { silent = true })
     vim.keymap.set("n", "<leader>nf", ":NewFile<CR>", { silent = true })
     vim.api.nvim_create_autocmd("FileType", {
-        pattern = "*",
-        callback = function()
-            local filetype = vim.bo.filetype
-            M.load_language_module(filetype)
-        end,
+	pattern = "*",
+	callback = function()
+	    local filetype = vim.bo.filetype
+	    M.load_language_module(filetype)
+	end,
     })
 end
 
